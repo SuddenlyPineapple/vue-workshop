483ae79ded1d271ae08d0d0da33c58bc2a7ed0ee
franciscoemanuel <francisco.emanuel@hotmail.com.br>
2020-10-16 00:29:13 -0300

    Adds computed property to encrypt and decrypt text example

--- src/components/4_Computed_Property.vue
+++ src/components/4_Computed_Property.vue
@@ -1,16 +1,44 @@
 <template>
-  <span>Rendering Time: {{ now }}</span>
+  <div>
+    <span>Automatically encrypt and decrypts a user input:</span>
+    <div v-show="encrypt">
+      <v-text-field v-model="userInput"></v-text-field>
+      <span>Result: {{ encryptedMessage }}</span>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   data: () => ({
-    firstName: "Jan",
-    lastName: "Kowalski"
+    userInput: "",
+    encrypt: true
   }),
+  methods: {
+    range(start, end) {
+      return Array.from(Array(end - start + 1), (_, n) => start + n);
+    },
+    transformStr(str, fn) {
+      return str
+        .split("")
+        .map(fn)
+        .join("");
+    }
+  },
   computed: {
-    fullName: () => {
-      return this.firstName + " " + this.lastName;
+    encryptedMessage() {
+      return this.transformStr(this.userInput, char =>
+        Object.keys(this.encryptDictionary).includes(char.toUpperCase())
+          ? String.fromCharCode(this.encryptDictionary[char.toUpperCase()])
+          : char
+      );
+    },
+    encryptDictionary() {
+      return this.range(65, 90).reduce((dictionary, charCode, index) => {
+        const letter = String.fromCharCode(charCode);
+        dictionary[letter] = this.range(65, 90).reverse()[index];
+        return dictionary;
+      }, {});
     }
   }
 };
